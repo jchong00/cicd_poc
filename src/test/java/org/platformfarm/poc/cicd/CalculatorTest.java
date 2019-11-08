a2a04798b67c7cbf55deb68239a3ba43070d0017
jchong <jchong00@gmail.com>
2019-11-08 09:57:36 +0900

    그래들에 jar 이름 명시

---
+++ src/test/java/org/platformfarm/poc/cicd/CalculatorTest.java
@@ -0,0 +1,16 @@
+package org.platformfarm.poc.cicd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void add() {
+
+        Calculator calc = new Calculator();
+        long expect = calc.add(1, 1);
+        Assert.assertEquals(2, expect);
+
+    }
+}
